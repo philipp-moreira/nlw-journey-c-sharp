2855f29d267271ec582f1e9f4078e81ad3038c20
Philipp Moreira <psmdeveloper@gmail.com>
2024-07-10 23:42:56 -0300

    feat ( Infrastructure/Data ) add mapping to Activities table
    class#2

--- src/Journey.Infrastructure/JourneyDbContext.cs
+++ src/Journey.Infrastructure/JourneyDbContext.cs
@@ -5,10 +5,16 @@ namespace Journey.Infrastructure;
 
 public class JourneyDbContext : DbContext
 {
-    public DbSet<Trip> Trips {get; set;}
+    public DbSet<Trip> Trips { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Data Source=/home/philipp/src/csharp/Journey/database/JourneyDatabase.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Activity>().ToTable("Activities");
+        base.OnModelCreating(modelBuilder);
     }
 }
