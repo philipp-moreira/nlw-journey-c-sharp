8b8b043661c15b744e4ad9c43170bc1a49ad3a52
Philipp Moreira <psmdeveloper@gmail.com>
2024-07-10 23:46:04 -0300

    feat (Api) add endpoints to travel entity to get all, get by id and remove and improved error handling
    - created specific endpoint to obtain all trips
    - created specific endpoint to obtain trip by id
    - created specific endpoint to remove a trip and its respective activities (in cascade)
    - removed error handling using try/catch, due to the inclusion of a specific filter for exceptions
    
    class#2

--- src/Journey.Api/Controllers/TripsController.cs
+++ src/Journey.Api/Controllers/TripsController.cs
@@ -1,50 +1,63 @@
-﻿using Journey.Application;
+﻿using Journey.Application.UseCases.Trips.GetAll;
+using Journey.Application.UseCases.Trips.GetById;
+using Journey.Application.UseCases.Trips.Register;
+using Journey.Application.UseCases.Trips.Remove;
 using Journey.Communication.Requests;
 using Journey.Communication.Responses;
-using Journey.Exception.ExceptionsBase;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Journey.Api;
+namespace Journey.Api.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
 public class TripsController : ControllerBase
 {
-    private readonly string GENERAL_ERROR_MESSAGE = "Unexpected error.";
-
     [HttpGet]
+    [ProducesResponseType<ResponseTripsJson>(StatusCodes.Status200OK)]
+    [ProducesResponseType<ResponseErrorsJson>(StatusCodes.Status500InternalServerError)]
     public IActionResult GetAll()
     {
-        try
-        {
-            var useCase = new GetAllTripUseCase();
-            var response = useCase.Execute();
+        var useCase = new GetAllTripUseCase();
+        var response = useCase.Execute();
 
-            return Ok(response);
-        }
-        catch
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, GENERAL_ERROR_MESSAGE);
-        }
+        return Ok(response);
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    [ProducesResponseType<ResponseTripJson>(StatusCodes.Status200OK)]
+    [ProducesResponseType<ResponseErrorsJson>(StatusCodes.Status404NotFound)]
+    [ProducesResponseType<ResponseErrorsJson>(StatusCodes.Status500InternalServerError)]
+    public IActionResult GetById([FromRoute] Guid id)
+    {
+        var useCase = new GetTripByIdUseCase();
+        var response = useCase.Execute(id);
+
+        return Ok(response);
     }
 
     [HttpPost]
+    [ProducesResponseType<ResponseTripJson>(StatusCodes.Status200OK)]
+    [ProducesResponseType<ResponseErrorsJson>(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType<ResponseErrorsJson>(StatusCodes.Status500InternalServerError)]
     public IActionResult Register([FromBody] RequestRegisterTripJson request)
     {
-        try
-        {
-            var useCase = new RegisterTripUseCase();
-            var result = useCase.Execute(request);
+        var useCase = new RegisterTripUseCase();
+        var result = useCase.Execute(request);
 
-            return Created(string.Empty, result);
-        }
-        catch (JourneyException ex)
-        {
-            return BadRequest(ex.Message);
-        }
-        catch
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, GENERAL_ERROR_MESSAGE);
-        }
+        return Created(string.Empty, result);
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType<ResponseErrorsJson>(StatusCodes.Status404NotFound)]
+    [ProducesResponseType<ResponseErrorsJson>(StatusCodes.Status500InternalServerError)]
+    public IActionResult Remove([FromRoute] Guid id)
+    {
+        var useCase = new RemoveByIdUseCase();
+        useCase.Execute(id);
+
+        return NoContent();
     }
 }
